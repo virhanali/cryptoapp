34cc269d2695b1839f25125fb09a80b23370d0ec
virhan <virhan.shoesmart@gmail.com>
2022-02-13 04:34:12 +0700

    implement reducer and consume api

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const { Title } = Typography;
 
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats
+  if (isFetching) return "Loading..."
   return (
     <>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
       <Row>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value="5" /></Col>
-        <Col span={12}><Statistic title="Total Exchange" value="5" /></Col>
-        <Col span={12}><Statistic title="Total Market Cap" value="5" /></Col>
-        <Col span={12}><Statistic title="Total 24H Volume" value="5" /></Col>
-        <Col span={12}><Statistic title="Total Markets" value="5" /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchange" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
+        <Col span={12}><Statistic title="Total 24H Volume" value={millify(globalStats.total24hVolume)} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
     </>
   )
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "b6d398b02bmshb6345ec3c03b299p1cf22djsnb2b9877bd2a6",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
